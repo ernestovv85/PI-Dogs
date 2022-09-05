85bacca8b0521ad763277d343e41ab10acfc1997
ernestovv85 <ernestovv85@gmail.com>
2022-09-05 12:39:28 -0500

    frontend iniciado

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage.jsx'
+import Home from './components/Home.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
